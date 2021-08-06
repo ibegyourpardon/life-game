c831bf2554748782e11c89f4c274ee19d4e36c32
feiandxs <yiziyint@gmail.com>
2021-08-06 15:27:42 +0800

    aha?

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="container">
     <h1>L G</h1>
-    <div></div>
+    <div>  {{ block_list }}  </div>
+    <div
+      :style="grid_container"
+    >
+      <div v-for="(grid, idx) in block_list" v-bind:key="idx" v-bind:style="grid || newbg" v-on:click="changebgcolor" class="grid" :id="idx"></div>
+
+    </div>
   </div>
 </template>
 
@@ -13,7 +19,29 @@ export default {
   },
   data() {
     return {
-      block_list: []
+      rows: 15,
+      styling: '  grid-template-columns: repeat(10, 10%);'
+    }
+  },
+  computed: {
+    block_list: function (){
+      let x = this.rows * this.rows
+      return x
+    },
+    grid_container: function (){
+      let a = {}
+      let v = (1 / this.rows) * 100
+      console.log(v)
+      let border = 'repeat(' + this.rows + ', ' + v +'%)'
+      a.width = '800px'
+      a.height = '800px'
+      a.display = 'grid'
+      // a.backgroundColor = 'red'
+      // a.gridTemplateColumns= 'repeat(3, 33.33%)'
+      a.gridTemplateColumns= border
+      a.gridTemplateRows=border
+
+      return a
     }
   }
 }
@@ -38,5 +66,22 @@ export default {
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
+}
+
+.grid-container {
+  /*display: flex;*/
+  /*flex-direction: row;*/
+  /*justify-content: flex-start;*/
+  /*flex-wrap: wrap;*/
+  /*align-items: flex-start;*/
+  display: grid;
+  /*grid-template-columns: repeat(10, 10%);*/
+}
+
+.grid {
+  width: 50px;
+  height: 50px;
+  background-color: #42b983;
+  margin: 0 5px 5px 0;
 }
 </style>
